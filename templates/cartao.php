d10ce39e866f1e01445b88a9163bd48282ce5dd6
git-leandroroman <leandro@ladamata.com>
2024-10-30 21:39:26 -0300

    291024

---
+++ templates/cartao.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Template Name: Cartao
+ */
+?>
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
+    <iframe src="https://sukhanegociosimobiliarios.com.br/wp-content/themes/sukha/templates/pdf/sukha-negocios-imobiliarios.pdf" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" allowfullscreen></iframe>
+</div>
